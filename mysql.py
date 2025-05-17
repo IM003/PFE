6381c57648f172496f08ebd85f1458835de69524
IM003 <i.mans00003@gmail.com>
2025-05-17 19:05:57 +0100

    login/register appPage

--- mysql.py
+++ mysql.py
@@ -1,1 +1,11 @@
+from sqlalchemy import create_engine
 
+db_path = 'sqlite:///test_alchemy.db'
+
+engine = create_engine(db_path)
+
+try: 
+    connection = engine.connect()
+    print("Success")
+except Exception as e:
+    print(e)
