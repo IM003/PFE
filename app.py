33badc70ff7f5b394bfbbd240503ca277bbedf44
IM003 <i.mans00003@gmail.com>
2025-05-21 10:04:21 +0100

    tentative auth et création du frontend

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, ValidationError
+import bcrypt
+from flask_mysqldb import MySQL 
 
 app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'mydatabase'
+app.secret_key = 'your_secret_key_here'
+
+mysql = MySQL(app)
+
+class RegisterForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    submit = SubmitField('Register')
 
 
 @app.route('/')
@@ -11,9 +31,33 @@ def home():
 def login():
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+    
+    print(f"mysql = {mysql}")  # pour debug
+    print(f"mysql.connection = {mysql.connection}")  # pour debug
+    
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()) 
+
+        try:
+            cursor = mysql.connection.cursor()
+            cursor.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", (name, email, hashed_password))
+            mysql.connection.commit()
+            cursor.close()
+        except Exception as e:
+            print(f"Erreur MySQL : {e}")
+            return "Erreur lors de l'enregistrement"
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
 
 
 if __name__ == '__main__':
